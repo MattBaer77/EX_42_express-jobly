554011a3252e7e606020259daa6e2173a7345cd4
Matthew Baer <matt.baer77@gmail.com>
2023-08-08 22:08:29 -0400

    sql.test.js completed

--- helpers/sql.test.js
+++ helpers/sql.test.js
@@ -1,33 +1,150 @@
 const { sqlForPartialUpdate } = require("./sql");
+const { BadRequestError } = require("../expressError");
+
 
 describe("sqlForPartialUpdate", function () {
 
-    test("works: ...", function () {
+    test("works: all inputs", function () {
 
         const dataToUpdate = { 
 
-            lastName: "Jordan",
             firstName: "Michael",
-
+            lastName: "Jordan"
 
         }
 
         const jsToSql = {
 
-            lastName: "last_name",
             firstName: "first_name",
-
+            lastName: "last_name"
 
         }
 
-        SQL = sqlForPartialUpdate(dataToUpdate, jsToSql)
+        reFormattedSql = sqlForPartialUpdate(dataToUpdate, jsToSql)
 
-        console.log(SQL)
-
-        expect(SQL).toBeTruthy
-
+        expect(reFormattedSql).toEqual(
+            {
+                setCols: '"first_name"=$1, "last_name"=$2',
+                values: [ 'Michael', 'Jordan' ]
+            }
+        )
 
     })
 
+    test("works: partial jsToSql", function () {
+
+        const dataToUpdate = { 
+
+            firstName: "Michael",
+            lastName: "Jordan"
+
+        }
+
+        const jsToSql = {
+
+            firstName: "first_name",
+
+        }
+
+        reFormattedSql = sqlForPartialUpdate(dataToUpdate, jsToSql)
+
+        console.log(reFormattedSql)
+
+        expect(reFormattedSql).toEqual(
+            {
+                setCols: '"first_name"=$1, "lastName"=$2',
+                values: [ 'Michael', 'Jordan' ]
+            }
+        )
+
+    })
+
+    test("works: no jsToSql", function () {
+
+        const dataToUpdate = { 
+
+            firstName: "Michael",
+            lastName: "Jordan"
+
+        }
+
+        const jsToSql = {
+
+        }
+
+        reFormattedSql = sqlForPartialUpdate(dataToUpdate, jsToSql)
+
+        console.log(reFormattedSql)
+
+        expect(reFormattedSql).toEqual(
+            {
+                setCols: '"firstName"=$1, "lastName"=$2',
+                values: [ 'Michael', 'Jordan' ]
+            }
+        )
+
+    })
+
+    test("works: partial dataToUpdate", function () {
+
+        const dataToUpdate = { 
+
+            firstName: "Michael",
+
+        }
+
+        const jsToSql = {
+
+            firstName: "first_name",
+            lastName: "last_name"
+
+        }
+
+        reFormattedSql = sqlForPartialUpdate(dataToUpdate, jsToSql)
+
+        expect(reFormattedSql).toEqual(
+            {
+                setCols: '"first_name"=$1',
+                values: [ 'Michael' ]
+            }
+        )
+
+    })
+
+})
+
+describe("sqlForPartialUpdate - Error Cases", function () {
+
+
+    test("doesn't work: no inputs", function () {
+
+        const dataToUpdate = { 
+
+        }
+
+        const jsToSql = {
+
+        }
+
+        expect(() => (sqlForPartialUpdate(dataToUpdate, jsToSql))).toThrow(new BadRequestError("No data"))
+
+    })
+
+    test("doesn't work: no inputs dataToUpdate", function () {
+
+        const dataToUpdate = { 
+
+        }
+
+        const jsToSql = {
+
+            firstName: "first_name",
+            lastName: "last_name"
+
+        }
+
+        expect(() => (sqlForPartialUpdate(dataToUpdate, jsToSql))).toThrow(new BadRequestError("No data"))
+
+    })
 
 })
