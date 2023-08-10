c7276211b9da94aede554f9588002edfb769f2a7
Matthew Baer <matt.baer77@gmail.com>
2023-08-09 23:17:10 -0400

    findNameLikeOnly tests completed

--- models/company.test.js
+++ models/company.test.js
@@ -236,6 +236,84 @@ describe("findAllFilter", function () {
 
 /************************************** findNameLikeOnly */
 
+describe("findNameLikeOnly", function () {
+
+  test("works: finds c1 with c1", async function () {
+
+    const data = {nameLike:"c1"}
+
+    let companies = await Company.findNameLikeOnly(data);
+    expect(companies).toEqual([
+      {
+        handle: "c1",
+        name: "C1",
+        description: "Desc1",
+        numEmployees: 1,
+        logoUrl: "http://c1.img",
+      }
+    ]);
+    
+  })
+
+  test("works: finds c2 with c2", async function () {
+
+    const data = {nameLike:"c2"}
+
+    let companies = await Company.findNameLikeOnly(data);
+    expect(companies).toEqual([
+      {
+        handle: "c2",
+        name: "C2",
+        description: "Desc2",
+        numEmployees: 2,
+        logoUrl: "http://c2.img",
+      }
+    ]);
+
+  })
+
+  test("works: finds all with c", async function () {
+
+    const data = {nameLike:"c"}
+
+    let companies = await Company.findNameLikeOnly(data);
+    expect(companies).toEqual([
+      {
+        handle: "c1",
+        name: "C1",
+        description: "Desc1",
+        numEmployees: 1,
+        logoUrl: "http://c1.img",
+      },
+      {
+        handle: "c2",
+        name: "C2",
+        description: "Desc2",
+        numEmployees: 2,
+        logoUrl: "http://c2.img",
+      },
+      {
+        handle: "c3",
+        name: "C3",
+        description: "Desc3",
+        numEmployees: 3,
+        logoUrl: "http://c3.img",
+      },
+    ]);
+
+  })
+
+  test("works: finds none with b", async function () {
+
+    const data = {nameLike:"b"}
+
+    let companies = await Company.findNameLikeOnly(data);
+    expect(companies).toEqual([]);
+
+  })
+
+})
+
 /************************************** findMaxEmployeesOnly */
 
 /************************************** findMinEmployeesOnly */
