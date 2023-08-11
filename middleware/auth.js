1a5c28cf86b5c53d823e0fad2096eae3c2ac4201
Matthew Baer <matt.baer77@gmail.com>
2023-08-10 20:24:54 -0400

    added ensureAdmin to auth.js

--- middleware/auth.js
+++ middleware/auth.js
@@ -18,9 +18,12 @@ const { UnauthorizedError } = require("../expressError");
 function authenticateJWT(req, res, next) {
   try {
     const authHeader = req.headers && req.headers.authorization;
+    console.log(authHeader)
     if (authHeader) {
       const token = authHeader.replace(/^[Bb]earer /, "").trim();
+      console.log(token)
       res.locals.user = jwt.verify(token, SECRET_KEY);
+      console.log(res.locals.user)
     }
     return next();
   } catch (err) {
@@ -42,8 +45,23 @@ function ensureLoggedIn(req, res, next) {
   }
 }
 
+/** Middleware to use when they must be logged in and isAdmin must be true.
+ *
+ * If not, raises Unauthorized.
+ */
+
+function ensureAdmin(req, res, next) {
+  try {
+    if (!res.locals.user.isAdmin) throw new UnauthorizedError();
+    return next();
+  } catch (err) {
+    return next(err);
+  }
+}
+
 
 module.exports = {
   authenticateJWT,
   ensureLoggedIn,
+  ensureAdmin,
 };
