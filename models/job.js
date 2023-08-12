5a2922aca97078f59e8531fd944fd690caf52f85
Matthew Baer <matt.baer77@gmail.com>
2023-08-11 23:48:33 -0400

    completed job.js Job.remove

--- models/job.js
+++ models/job.js
@@ -176,22 +176,22 @@ class Job {
     return job;
   }
 
-//   /** Delete given company from database; returns undefined.
-//    *
-//    * Throws NotFoundError if company not found.
-//    **/
+  /** Delete given job from database; returns undefined.
+   *
+   * Throws NotFoundError if company not found.
+   **/
 
-//   static async remove(handle) {
-//     const result = await db.query(
-//           `DELETE
-//            FROM companies
-//            WHERE handle = $1
-//            RETURNING handle`,
-//         [handle]);
-//     const company = result.rows[0];
+  static async remove(id) {
+    const result = await db.query(
+          `DELETE
+           FROM jobs
+           WHERE id = $1
+           RETURNING id`,
+        [id]);
+    const job = result.rows[0];
 
-//     if (!company) throw new NotFoundError(`No company: ${handle}`);
-//   }
+    if (!job) throw new NotFoundError(`No company: ${id}`);
+  }
 }
 
 
